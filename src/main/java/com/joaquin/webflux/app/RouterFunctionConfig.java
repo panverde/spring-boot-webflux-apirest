47b6ac4b505fc1f3d108022fe0664f72d32a8e5a
Joaquin Alberto Huauya Arontico <jhuauyaa@everis.com>
2019-10-28 18:00:07 -0500

    agregando con eureka

--- src/main/java/com/joaquin/webflux/app/RouterFunctionConfig.java
+++ src/main/java/com/joaquin/webflux/app/RouterFunctionConfig.java
@@ -1,6 +1,9 @@
 package com.joaquin.webflux.app;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.PUT;
+import static org.springframework.web.reactive.function.server.RequestPredicates.DELETE;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 import org.springframework.context.annotation.Bean;
@@ -16,9 +19,14 @@ public class RouterFunctionConfig {
 	@Bean
 	public RouterFunction<ServerResponse> routes(ProductoHandler handler){
 		
-		return route(GET("/api/v2/productos").or(GET("/api/v3/productos")),handler::listar);
-		
-	}
+		return route(GET("/api/v2/productos").or(GET("/api/v3/productos")),handler::listar)
+				.andRoute(GET("/api/v2/productos/{id}"), handler::ver)
+				.andRoute(POST("/api/v2/productos"), handler::crear)
+				.andRoute(PUT("/api/v2/productos/{id}"), handler::editar)
+				.andRoute(DELETE("/api/v2/productos/{id}"), handler::eliminar)	
+				.andRoute(POST("/api/v2/productos/upload/{id}"), handler::upload)	
+				.andRoute(POST("/api/v2/productos/crear"), handler::crearConFoto);	
+	}	
 	
 
 }
